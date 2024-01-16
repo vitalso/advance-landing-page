880bd89cc6059fd98a98d7efbd8639ada1732834
Vitaliy <lgphone.vs@gmail.com>
2024-01-16 15:05:54 +0200

    fix the link

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,12 +8,12 @@
 
             <div class="text-base lg:text-lg w-2/3 sm:w-auto">
                 <p class="mb-2 leading-7 font-medium">Get in touch</p>
-                <NuxtLink to="mailto: hello@advance.com" class="hover:text-primary-light transition-all">hello@advance.com</NuxtLink>
+                <NuxtLink to="mailto: hello@advance.com" class="hover:text-primary-light transition-all">hello@advancefounders.com</NuxtLink>
             </div>
 
             <div class="text-base lg:text-lg w-1/3 sm:w-auto">
                 <p class="mb-3 leading-7 font-medium">Follow Us</p>
-                <NuxtLink to="https://www.linkedin.com/company/101489336/admin/feed/posts/?feedType=following" targey="_blank">
+                <NuxtLink to="https://www.linkedin.com/company/101489336/admin/feed/posts/?feedType=following" target="_blank">
                     <nuxt-icon name="linked-in" filled class="fill-white hover:fill-primary-light transition-all" />
                 </NuxtLink>
             </div>
